c26aa0aaa24fdf719060649e8140f0a3a67aca96
Timothy Lee <tlee87@gmail.com>
2016-08-18 21:48:57 -0400

    Added users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160810040848) do
+ActiveRecord::Schema.define(version: 20160819011731) do
 
   create_table "cardlocations", force: :cascade do |t|
     t.integer  "card_id"
@@ -52,5 +52,15 @@ ActiveRecord::Schema.define(version: 20160810040848) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
